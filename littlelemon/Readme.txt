b853bb016c6363a66c9b43dc4c15e3e886da40c7
CourseTjas <teoprova10@gmail.com>
2024-05-03 10:16:49 +0200

    Readme

---
+++ littlelemon/Readme.txt
@@ -0,0 +1,7 @@
+restaurant/api-token-auth/
+restaurant/booking/tables/
+restaurant/menu/items
+restaurant/menu/items/<int:pk>
+
+REGISTER: POST ->  /auth/users/
+All USERS: GET -> /auth/users/
--- littlelemon/restaurant/urls.py
+++ littlelemon/restaurant/urls.py
@@ -5,6 +5,6 @@ from rest_framework.authtoken.views import obtain_auth_token
 urlpatterns = [
     path('', views.index, name='index'),
     path('menu/items', views.MenuItemView.as_view()),
-    path('menu/<int:pk>', views.SingleMenuItemView.as_view()),
+    path('menu/items/<int:pk>', views.SingleMenuItemView.as_view()),
     path('api-token-auth/', obtain_auth_token),
 ]
